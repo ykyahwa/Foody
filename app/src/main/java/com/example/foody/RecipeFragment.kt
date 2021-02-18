b993c94214e860667c83070ba5ee1beb78beb8ac
EokHyun Lee <ykyahwa@gmail.com>
2021-02-19 07:42:31 +0900

    section 3 - Design RecipesFragment Layout

--- app/src/main/java/com/example/foody/RecipeFragment.kt
+++ app/src/main/java/com/example/foody/RecipeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_recipe.view.*
 
 class RecipeFragment : Fragment() {
 
@@ -13,7 +14,11 @@ class RecipeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipe, container, false)
+
+        view.recyclerview.showShimmer()
+
+        return view
     }
 
 }
